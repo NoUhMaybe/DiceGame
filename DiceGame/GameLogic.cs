0333f8b601bcf37f0c7061c74d2caa6b944c4f8e
NoUhMaybe <nouhmaybe@outlook.com>
2025-02-07 16:40:58 -0600

    Finished Game

---
+++ DiceGame/GameLogic.cs
@@ -0,0 +1,34 @@
+﻿
+public class GameLogic
+{
+    public static void StartGame()
+    {
+        var num = new Random().Next(1, 7);
+        var guesses = 3;
+        Console.WriteLine(GameOutput.GameStart);
+        while (guesses > 0)
+        {
+            Console.WriteLine(GameOutput.PromptUserForGuess);
+            string userInput = Console.ReadLine();
+            bool TryParse = int.TryParse(userInput, out int guess);
+            if (guess == num)
+            {
+                Console.WriteLine(GameOutput.WinMessage);
+                return;
+            }
+            else if (!TryParse || num < 0 || num > 6)
+            {
+                continue;
+            }
+            else
+            {
+                Console.WriteLine(GameOutput.LoseMessage);
+                guesses--;
+            }
+
+        }
+        Console.WriteLine(GameOutput.GameLost);
+        Console.WriteLine($"Number was {num}");
+        return;
+    }
+}
---
+++ DiceGame/GameOutput.cs
@@ -0,0 +1,9 @@
+﻿
+public class GameOutput
+{
+    public static readonly string WinMessage = "You win!";
+    public static readonly string LoseMessage = "Wrong number.";
+    public static readonly string PromptUserForGuess = "Enter a number: ";
+    public static readonly string GameStart = "Dice Rolled. Guess a number 1-6.\nYou have 3 guesses.";
+    public static readonly string GameLost = "You lose :(";
+}
